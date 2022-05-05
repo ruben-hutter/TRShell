2f21be31a93679df14fac6eb604a6d913a383214
Tobias Hafner <Tobias_Hafner@live.de>
2022-05-05 11:53:44 +0200

    refactored wrapper

--- trsh_py/wrapper.py
+++ trsh_py/wrapper.py
@@ -7,20 +7,21 @@ from time import sleep
 SHELL_EXEC_PATH = "dummy_shell.o"
 
 class Wrapper:
-    # pushes a string to the qui
-    def push_to_gui(self, output_string):
 
-        # debug printout
-        print(f"Subprocess sent: {output_string}")
+    # construct new trshell object
+    def __init__(self, gui_section):
+        self.gui_section = gui_section
+        self.shell = None
+        self.run_shell()
 
-    # reads the str out of the shell process and calls push_to_gui
-    def read_shell_stdout(self):
-        while True:
-            output_string = self.shell.stdout.read(1).decode()
-            if output_string == '' and self.shell.poll() != None:
-                break
-            self.push_to_gui(output_string)
-
+    # terminate terminal on destruction of wrapper
+    def __del__(self):
+        if self.shell == None:
+            return
+        self.shell.terminate()
+        if self.shell != None and self.shell.poll() == None:
+            self.shell.kill()
+    
     # launches a new trshell c process
     def run_shell(self):
         # launch shell c with subprocess 
@@ -39,21 +40,21 @@ class Wrapper:
         sleep(10)
         stdout_reader.start()
 
+    # reads the str out of the shell process and calls push_to_gui
+    def read_shell_stdout(self):
+        while True:
+            output_string = self.shell.stdout.read(1).decode()
+            if output_string == '' and self.shell.poll() != None:
+                break
+            self.push_to_gui(output_string)
+
+    # pushes a string to the qui
+    def push_to_gui(self, output_string):
+
+        # debug printout
+        print(f"Subprocess sent: {output_string}")
+
     # pushes a string to the trshell c process
     def push_to_shell(self, input_string):
         self.shell.communicate(input=input_string.encode())
         return
-        
-    # construct new trshell object
-    def __init__(self, gui_section):
-        self.gui_section = gui_section
-        self.shell = None
-        self.run_shell()
-
-    # terminate terminal on destruction of wrapper
-    def __del__(self):
-        if self.shell == None:
-            return
-        self.shell.terminate()
-        if self.shell != None and self.shell.poll() == None:
-            self.shell.kill()
