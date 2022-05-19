2c133fd22bdfbb6f05402d57c27f8e783797a040
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-05-19 16:35:56 +0200

    get pwd in promt

--- shell/prompt.c
+++ shell/prompt.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "colors.h"
 #include "shell.h"
-# include <stdlib.h>
 
 #define PROMPT_1 "tobi :: TRShell ᐅ $ "
 #define PROMPT_2 "> "
+
+char* current_working_dir[100];
 
 void print_prompt_1(void) {
     char* prompt_string = colorize(PROMPT_1, ORANGE);
@@ -16,4 +20,12 @@ void print_prompt_2(void) {
     char* prompt_string = colorize(PROMPT_2, ORANGE);
     fprintf(stderr, prompt_string);
     free(prompt_string);
+}
+
+void get_pwd(void) {
+    if (getcwd(current_working_dir, sizeof(current_working_dir)) != NULL) {
+        return;
+    }
+    perror("getcwd() error");
+    return NULL;
 }
