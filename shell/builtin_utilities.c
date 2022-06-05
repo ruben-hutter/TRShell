a870a4b878fa38ba19e3302873703eff1b8187e5
Tobias Hafner <Tobias_Hafner@live.de>
2022-06-05 10:49:23 +0200

    fix extern error in builtin utils

--- shell/builtin_utilities.c
+++ shell/builtin_utilities.c
@@ -5,4 +5,4 @@ struct builtin_utility builtin_utilities[] =
         {"dump", dump},
     };
 
-extern int builtin_utility_count = sizeof(builtin_utilities)/sizeof(struct builtin_utility);
+int builtin_utility_count = sizeof(builtin_utilities)/sizeof(struct builtin_utility);
