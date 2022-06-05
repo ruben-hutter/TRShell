468986c36c6d7249147bc16fc2640ff6109d8934
Tobias Hafner <Tobias_Hafner@live.de>
2022-06-05 14:34:46 +0200

    add character based input reader

--- shell/input_reader.h
+++ shell/input_reader.h
@@ -11,4 +11,18 @@
 
 #define READ_BUFFER_SIZE 1024
 
+// reads user input from the stdin
+char* read_from_input();
+
+int get_string_from_input(char* buffer, int buffer_size);
+
+// hande control sequence
+void handle_control_sequence(char* sequence);
+
+/*set tty mode to raw*/
+void set_tty_raw();
+
+/*restore the old settings*/
+void restore_old_tty_settings();
+
 #endif
