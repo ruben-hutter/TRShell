523416878038ff832ce3b584873452f8605d70df
Tobias Hafner <Tobias_Hafner@live.de>
2022-06-13 09:51:13 +0200

    header now matching impl

--- shell/string_utils.h
+++ shell/string_utils.h
@@ -8,6 +8,8 @@
 #include <ctype.h>
 #include <stdbool.h>
 
+#include "string_utils.h"
+
 // get the length of all passed string concatenated
 int get_concatenated_length(int num_of_args, ...);
 
@@ -18,8 +20,8 @@ int v_get_concatenated_length(int num_of_args, va_list input_strings);
 // note that output_string must be of succficient length to fit concatenation
 void concatenate(int num_of_args, char* output_string, ...);
 
-// concatenate all strings in the va_listin to output string
-// note that output_string must be of succficient length to fit concatenation
+// concatenate all strings in the va_list to output string
+// note that output_string must be of sufficient length to fit concatenation
 void v_concatenate(int num_of_args, char* output_string, va_list input_strings);
 
 // malloc an empty string of the specified length
@@ -35,27 +37,21 @@ void make_empty_string(char* input_string);
 // example for delimiter '&': first&second&third -> third
 void crop_string_to_end(char* input_string, char delimiter);
 
-// delete the char at the specified position
-void delete_char_at(char* input_string, size_t char_index);
-
-// subsitiutes the section between start and end of first_string with the second_string
-char* subsitute_section_with_string(char* input_string, char* substitute_string, size_t start, size_t end);
-
-// n puts a string in quotes
-char* add_quotes(char* input_string, int add_quotes);
-
-int check_buffer_bounds(int* count, int* length, char*** buffer);
-
-void free_buffer(int length, char** buffer);
-
+// sets a terminator at the position of the first newline
 void cut_at_trailing_newline(char* input_string);
 
+// returs true if a string is only whitespace, otherwise false
 int is_only_whitespace(const char* input_string);
 
+// shifts the section form reg_start to reg_end one character to the right, leading to a gap at region_start
+// indexes are not checked!
 void shift_string_right(char* input_string, int region_start, int region_end);
 
+// shifts the section form reg_start to reg_end one character to the left, leading to the char at reggion_start to be overwritten
+// indexes are not checked!
 void shift_string_left(char* input_string, int region_start, int region_end);
 
+// put a the section from form reg_start to reg_end to the std out using putchar
 void put_string_section(char* input_string, int region_start, int region_end);
 
 // searches the list for a unique possible match of the approach and returns the match
