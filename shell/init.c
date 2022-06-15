df078fe94e43a44d5c7e9e2d007b27f7e898072e
ruben.hutter <ruben.hutter@stud.unibas.ch>
2022-06-15 02:12:11 +0200

    refactored init

--- shell/init.c
+++ shell/init.c
@@ -6,11 +6,11 @@ void init_shell() {
     init_table();
     // add environment variables to symbol table
     struct symbol_table_entry* entry;
-    char **p2 = environ;
+    char** p2 = environ;
     while(*p2) {
-        char *eq = strchr(*p2, '=');
+        char* eq = strchr(*p2, '=');
         if (eq) {
-            int len = eq-(*p2);
+            int len = eq - (*p2);
             char name[len + 1];
             strncpy(name, *p2, len);
             name[len] = '\0';
